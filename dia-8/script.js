d603675efc059f020bba16976f6bd0acd06acd86
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-02-23 01:17:58 -0300

    calculadora dodev1

--- dia-8/script.js
+++ dia-8/script.js
@@ -2,10 +2,11 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
-function calculadora(numero1, numero2, operador){
+function Calculadora(numero1, numero2, operador){
   let resultado 
-  let num1 = perseInt(numero1)
-  let num2 = perseInt(numero2)
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+
   switch (operador) {
     case "+":
     resultado = num1 + num2
@@ -21,7 +22,7 @@ function calculadora(numero1, numero2, operador){
       break;
     case "e":
     let contador = 1
-    resultado = num1 * num2
+    resultado = num1 * num1
     while(contador < num2 - 1){
       resultado = resultado * num1
       contador++
