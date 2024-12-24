4c923165ccfe0898a9cc3f0eb050e7134acdb820
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-12-23 22:48:24 -0300

    exercicio 1 de lógica

--- exercicio-1/ex-1.js
+++ exercicio-1/ex-1.js
@@ -1,7 +1,10 @@
+let num1 = 2
+let num2 = 4
 
-
-
-
+console.log("2 + 4 = " + (num1 + num2))
+console.log("2 - 4 = " + (num1 - num2))
+console.log("2 / 4 = " + (num1 / num2))
+console.log("2 * 4 = " + (num1 * num2))
 
 
 
