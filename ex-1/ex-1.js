00f6a5962dd87f4a4e6d407ccb126a40cb5f1218
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-12-23 23:50:17 -0300

    2 exercicios feitos

--- ex-1/ex-1.js
+++ ex-1/ex-1.js
@@ -1,5 +1,5 @@
 let num1 = 2
-let num2 = 4
+let num2 = 4 
 
 console.log("2 + 4 = " + (num1 + num2))
 console.log("2 - 4 = " + (num1 - num2))
---
+++ ex-2/ex-2.js
@@ -0,0 +1,14 @@
+
+
+let valor_jantar = 400
+let taxa_garcom = 10
+let total
+
+total = valor_jantar + ((taxa_garcom / 100) * valor_jantar)
+
+console.log("Valor do Jantar: R$ " + valor_jantar) 
+console.log(`Valor taxa do garçom: R$${((taxa_garcom / 100) * valor_jantar)}`)
+console.log(`Valor total do jantar: R$ ${total}`)
+
+
+
