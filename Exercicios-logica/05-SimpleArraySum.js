ed2d34ebac399a7f14939ab86844e6819dc8afcc
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-02 11:57:32 -0300

    05- Simple array sum feito

---
+++ Exercicios-logica/05-SimpleArraySum.js
@@ -0,0 +1,19 @@
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum(ar) {
+    // Write your code here
+    let soma = 0;
+    
+    for(let i = 0; i < ar.length; i++){
+        soma = soma + ar[i];
+    }
+    
+    return soma;
+}
+
+console.log(simpleArraySum([1, 2, 3, 4]));
