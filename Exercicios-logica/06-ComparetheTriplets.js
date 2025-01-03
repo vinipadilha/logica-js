a4bdbd49f15765e829fae04fcf99a58c26c82ada
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-03 00:07:35 -0300

    trocando nome de variavel (let) i para index

--- Exercicios-logica/06-ComparetheTriplets.js
+++ Exercicios-logica/06-ComparetheTriplets.js
@@ -1,20 +1,12 @@
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
 
 function compareTriplets(a, b) {
     let alice = 0
     let bob = 0 
 
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] > b[i] ){
+    for (let index = 0; index < a.length; index++) {
+        if (a[index] > b[index] ){
             alice += 1
-        } else if (a[i] < b[i]){
+        } else if (a[index] < b[index]){
             bob += 1
         }
     }
