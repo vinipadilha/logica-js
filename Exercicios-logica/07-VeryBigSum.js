7b472861793bc461067e6cd9efdf11c3f0a728e3
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-05 09:53:36 -0300

    a very big sum complete

--- Exercicios-logica/07-VeryBigSum.js
+++ Exercicios-logica/07-VeryBigSum.js
@@ -1,6 +1,13 @@
 function aVeryBigSum(ar) {
     // Write your code here
+    soma = 0
 
+    for (index = 0; index < ar.length; index++){
+        soma = soma + ar[index]
+    }
+
+    return soma
 }
 
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
 
