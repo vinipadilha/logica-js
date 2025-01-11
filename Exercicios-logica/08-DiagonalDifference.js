35e6a75bbcd1fa31b2cf7a859622302d8f173dac
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-11 00:17:16 -0300

    ex08 completo

--- Exercicios-logica/08-DiagonalDifference.js
+++ Exercicios-logica/08-DiagonalDifference.js
@@ -2,12 +2,20 @@ function diagonalDifference(arr) {
     // Write your code here
     let somaL = 0;
     let somaR = 0;
+    let difference = 0;
 
     for (let index = 0; index < arr.length; index++) {
         somaL += arr[index][index]
-        
+        somaR += arr[index][arr.length - 1 - index]
+    }
+
+    if (somaL < somaR){
+        difference = somaR - somaL
+    } else {
+        difference = somaL- somaR
     }
     
+    return difference;
     
 }
 
