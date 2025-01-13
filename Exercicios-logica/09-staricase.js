7a337c80bd5f637b83a152ce54bd128285613c20
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-12 23:44:12 -0300

    ex 09

--- Exercicios-logica/09-staricase.js
+++ Exercicios-logica/09-staricase.js
@@ -2,6 +2,11 @@ function staircase(n) {
     // Write your code here
     let symbol = '#'
     let inputLine = ''
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
 }
 
 console.log(staircase(4))
