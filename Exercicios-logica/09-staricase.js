60f4046def2f10d99ca556a9e333842e40b54cd1
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-14 23:43:22 -0300

    exercicio feito staircase

--- Exercicios-logica/09-staricase.js
+++ Exercicios-logica/09-staricase.js
@@ -2,9 +2,24 @@ function staircase(n) {
     // Write your code here
     let symbol = '#';
     let inputLine = '';
+    let inputPosition = n - 1
 
+    for(let lineIndex = 0; lineIndex < n ; lineIndex++){
+        
+        for(let columnIndex = 0; columnIndex < n; columnIndex++){
+            if(columnIndex < inputPosition){
+                inputLine += ' '; 
+            } else {
+                inputLine += symbol;
+            }
+        }
+        console.log(inputLine);
+        inputLine = '';
+        inputPosition -= 1
+    }
     
-}
-
+} 
 console.log(staircase(4))
+
+
 
