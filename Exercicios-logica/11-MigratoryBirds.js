62eba6c12c6e9446c9288ab6697d9ac3dc84fe04
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-25 20:21:53 -0300

    desenvolvendo migratory birds por função for each

--- Exercicios-logica/11-MigratoryBirds.js
+++ Exercicios-logica/11-MigratoryBirds.js
@@ -1,11 +1,14 @@
 function migratoryBirds(arr) {
     // Write your code here
-    let conts = 
+    let counts = {};
+
+    arr.array.forEach((count) => {
+        (counts[count] || 0) + 1;
+        
+    });
     
 
 }
-
-
 
 
 console.log(migratoryBirds(1, 4, 4, 4, 5, 3));
