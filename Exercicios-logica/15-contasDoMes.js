8f231a40ad8cccfca2abeca667a13d6967002b2f
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-28 12:09:52 -0300

    Exercicio de controle financeiro mensal feito

---
+++ Exercicios-logica/15-contasDoMes.js
@@ -0,0 +1,13 @@
+let salario = 1200;
+let agua = 50;
+let luz = 60;
+let internet = 60;
+let gas = 100
+
+let gastos = agua + luz + internet + gas
+
+console.log(`Salário: R$${salario}
+Total de contas: R$${gastos}
+Sobrou: R$ ${salario - gastos}`)
+
+
