da23b87f59491bda019501308ceedfba668675f1
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-01-29 22:08:41 -0300

    Exercicio contas do mes feito

--- Exercicios-logica/15-contasDoMes.js
+++ Exercicios-logica/15-contasDoMes.js
@@ -1,13 +1,19 @@
 let salario = 1200;
-let agua = 50;
-let luz = 60;
-let internet = 60;
-let gas = 100
+let gastos = {
+    'Aluguel' : 800,
+    'Luz' : 80,
+    'Água' : 30,
+    'Internet' : 90,
+};
 
-let gastos = agua + luz + internet + gas
+let totalGasto = 0
+
+totalGasto = Object.values(gastos).reduce((a,b) => a + b);
+
 
 console.log(`Salário: R$${salario}
 Total de contas: R$${gastos}
-Sobrou: R$ ${salario - gastos}`)
+Sobrou: R$ ${salario - totalGasto}`)
+
 
 
