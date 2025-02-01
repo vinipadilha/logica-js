b43205d081954ea93c5247fab89c426d17065a12
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-01 14:46:04 -0300

    ex enade de forma otimizada

--- Exercicios-logica/16-2-ENADEADS21.js
+++ Exercicios-logica/16-2-ENADEADS21.js
@@ -1,32 +1,32 @@
 let alturas = [];
-function criaPessoa (num){
-    for(let index = 0; index < num; index += 1){
-        alturas.push((Math.random() * (2.5 - 1.3) + 1.3).toFixed(2))
+
+function criaPessoa(num) {
+    for (let index = 0; index < num; index++) {
+        alturas.push(Number((Math.random() * (2.5 - 1.3) + 1.3).toFixed(2)));
     }
 }
 
+criaPessoa(1000);
 
-criaPessoa(10)
 
+alturas.sort((a, b) => a - b);
 
-// deixar array em ordem crescente para descobrir maior e menor 
 let soma = 0;
 
-alturas.forEach((altura) => soma += altura);
+alturas.forEach(altura => soma += altura);
 
-const media = (soma / alturas.length).toFixed(2)
+const media = (soma / alturas.length).toFixed(2);
 
 
 let menorMedia = 0;
-alturas.forEach((altura) => {
-    if(altura < media){
-        menorMedia ++
-    } 
-})
+alturas.forEach(altura => {
+    if (altura < Number(media)) { 
+        menorMedia++;
+    }
+});
 
 
-console.log(`Menor altura: ${(alturas.sort()[0]).toFixed(2)}`)
-console.log(`Maior altura: ${(alturas.sort((a, b) => b - a)[0]).toFixed(2)}`)
-console.log(`Média das alturas: ${media}`)
-console.log(`Pessoas abaixo da média: ${menorMedia}`)
-
+console.log(`Menor altura: ${alturas[0].toFixed(2)}`);
+console.log(`Maior altura: ${alturas[alturas.length - 1].toFixed(2)}`);
+console.log(`Média das alturas: ${media}`);
+console.log(`Pessoas abaixo da média: ${menorMedia}`);
