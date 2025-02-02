74ede8e7bfbe8e0a23e6b2d1beb922e73870e66e
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-02 14:06:33 -0300

    ex enade em codigo otimizado

--- Exercicios-logica/16-2-ENADEADS21.js
+++ Exercicios-logica/16-2-ENADEADS21.js
@@ -1,6 +1,23 @@
 let alturas = [1.80, 1.67, 1.90, 2.03];
 
 // deixar array em ordem crescente para descobrir maior e menor 
+let soma = 0;
+
+alturas.forEach((altura) => soma += altura);
+
+const media = soma / alturas.length
+
+
+let menorMedia = 0;
+alturas.forEach((altura) => {
+    if(altura < media){
+        menorMedia ++
+    } 
+})
+
+
 console.log(`Menor altura: ${alturas.sort()[0]}`)
 console.log(`Maior altura: ${alturas.sort((a, b) => b - a)[0]}`)
- 
+console.log(`Média das alturas: ${media.toFixed(2)}`)
+console.log(`Pessoas abaixo da média: ${menorMedia}`)
+
