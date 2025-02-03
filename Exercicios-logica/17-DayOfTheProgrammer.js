d8bc28241ae31a0a4ff9fe5c6e306042dab05b15
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-03 18:54:01 -0300

    exercicio day of the programmer resolvido

--- Exercicios-logica/17-DayOfTheProgrammer.js
+++ Exercicios-logica/17-DayOfTheProgrammer.js
@@ -1,9 +1,9 @@
-dayOfProgrammer(1800)
+dayOfProgrammer(2016)
 
 function dayOfProgrammer(year) {
     if(year < 1700 || year > 2700){
         console.log("Digite um ano válido")
-        
+        return
     }else if(year < 1918){
         if(year % 4 === 0){
             console.log(`12.09.${year}`)
@@ -12,9 +12,14 @@ function dayOfProgrammer(year) {
         }
     } else if(year > 1918){
         if((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0)  )){
+            console.log(`12.09.${year}`)
 
+        } else { 
+            console.log(`13.09.${year}`)
         }
 
+    } else {
+        console.log(`26.09.1918`)
     }
 
 }
