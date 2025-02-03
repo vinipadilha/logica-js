32aaee89a8727fb6ddcc8e18438b467673b23462
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-03 19:31:44 -0300

    exercicio day of the programmer para o hacker rank

--- Exercicios-logica/17-DayOfTheProgrammer.js
+++ Exercicios-logica/17-DayOfTheProgrammer.js
@@ -1,25 +1,20 @@
 dayOfProgrammer(2016)
-
 function dayOfProgrammer(year) {
-    if(year < 1700 || year > 2700){
-        console.log("Digite um ano válido")
-        return
-    }else if(year < 1918){
-        if(year % 4 === 0){
-            console.log(`12.09.${year}`)
+    if (year < 1700 || year > 2700) {
+        return "Digite um ano valido"; 
+    } else if (year < 1918) {
+        if (year % 4 === 0) {
+            return "12.09." + year;
         } else {
-            console.log(`13.09.${year}`)
+            return "13.09." + year;
         }
-    } else if(year > 1918){
-        if((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0)  )){
-            console.log(`12.09.${year}`)
-
-        } else { 
-            console.log(`13.09.${year}`)
+    } else if (year > 1918) {
+        if ((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))) {
+            return "12.09." + year;
+        } else {
+            return "13.09." + year;
         }
-
     } else {
-        console.log(`26.09.1918`)
+        return "26.09.1918";
     }
-
 }
