4c4221a1e27ab7e568a4f420feb285fdf0dffd66
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-06 01:56:21 -0300

    tentativa de fazer o exercicio breaking the records

--- Exercicios-logica/19-BreakingTheRecords.js
+++ Exercicios-logica/19-BreakingTheRecords.js
@@ -1,7 +1,23 @@
+
+
 function breakingRecords(scores) {
     // Write your code here
+    let min = scores[0]
+    let max = scores[0]
 
-}
+    for(let index = 1; index < scores.lenght; index += 1){
+
+        if(scores[index] > max) {
+            max++
+        } else if(scores[index] < min){
+            min++
+        } 
+
+    }
+
+    return max, min
+
+};
 
 
 console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]))
