1c60aa7d3e9e6657bd771bd164bd8bcd60be7b76
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-06 02:15:43 -0300

    Exercicio divisible sum pairs feito com sucesso

---
+++ Exercicios-logica/20-DivisibleSumPairs.js
@@ -0,0 +1,27 @@
+
+function divisibleSumPairs(n, k, arr) {
+    // Write your code here
+    let pairs = 0;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+
+            if ((i < j) && ((arr[i] + arr[j]) % k === 0)) {
+                pairs++
+                
+            }
+
+            
+        }
+        
+    }
+    return pairs;
+
+}
+
+console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
+
+
+
+
+
