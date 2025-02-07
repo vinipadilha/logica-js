e78de324fde4713a1240074f06a845b44ac17d07
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-07 00:27:56 -0300

    exercicio finalizado

--- Exercicios-logica/22-BirthdayCakeCandles.js
+++ Exercicios-logica/22-BirthdayCakeCandles.js
@@ -4,19 +4,24 @@ function birthdayCakeCandles(candles) {
     let bigger = 0;
 
     for (let index = 0; index < candles.length; index++) {
-        
         if (candles[index] > bigger) {
-            bigger = candles[index]  
+            bigger = candles[index]
             
-        } else if(candles[index - 1] == bigger) {
-            sum++ 
         }
         
     }
+
+    for (let i = 0; i < candles.length; i++) {
+        if (bigger == candles[i]) {
+            sum++
+            
+        }
+        
+    }
+
     return sum;
 }
 
 
 console.log(birthdayCakeCandles([3, 2, 1, 3]));
 console.log(birthdayCakeCandles([18, 90, 90, 13, 90, 75, 90, 8, 90, 43]));
-console.log(birthdayCakeCandles([3, 2, 1, 3]));
