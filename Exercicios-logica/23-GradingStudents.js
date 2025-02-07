7585186743719aefb8c07bc072bba73dec070e32
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-07 01:50:12 -0300

    tentativa de fazer exercicio grading students

--- Exercicios-logica/23-GradingStudents.js
+++ Exercicios-logica/23-GradingStudents.js
@@ -1,9 +1,28 @@
 function gradingStudents(grades) {
     // Write your code here
+    let notas = [];
+    let sobra = 0;
+    let valorFinal = 0;
+    
+
+    for (let i = 0; i < grades.length; i++) {
+        
+        if(grades[i] < 38 || grades[i] % 5 === 0){
+            notas.push(grades[i])
+
+        } else {
+            sobra = grades[i] % 5 
+            valorFinal = grades[i] + sobra
+            notas.push(valorFinal)
+
+        }
+        
+    }
+    return notas
 
 }
 
 
-
+console.log(gradingStudents([73, 67, 38, 33]))
 
 
