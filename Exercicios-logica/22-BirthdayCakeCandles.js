5f5c207e2a800fbadd66302fd2b89f91d464d3ee
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-07 01:25:10 -0300

    birthday cake candles de forma mais eficiente

--- Exercicios-logica/22-BirthdayCakeCandles.js
+++ Exercicios-logica/22-BirthdayCakeCandles.js
@@ -1,3 +1,4 @@
+/*
 function birthdayCakeCandles(candles) {
     // Write your code here
     let sum = 0;
@@ -21,7 +22,33 @@ function birthdayCakeCandles(candles) {
 
     return sum;
 }
+*/
+
+
+// console.log(birthdayCakeCandles([3, 2, 1, 3]));
+// console.log(birthdayCakeCandles([18, 90, 90, 13, 90, 75, 90, 8, 90, 43]));
+
+
+function birthdayCakeCandles(candles) {
+    candles.sort((a, b) => a - b);
+    let sum = 0;
+
+    let bigger = candles[candles.length - 1]
+
+    for (let index = 0; index < candles.length; index++) {
+        
+        if (candles[index] == bigger) {
+            sum++
+            
+        }
+        
+    }
+    return sum;
+}
+
 
 
 console.log(birthdayCakeCandles([3, 2, 1, 3]));
 console.log(birthdayCakeCandles([18, 90, 90, 13, 90, 75, 90, 8, 90, 43]));
+
+
