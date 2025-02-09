b2fd641a4e0efdc15c178b9a2b6d79cffc5a57da
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-09 12:20:14 -0300

    Fazendo exercicio number line jumps

---
+++ Exercicios-logica/25-NumberLineJumps.js
@@ -0,0 +1,14 @@
+function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+    
+
+
+    if (x1 += v1 == x2 += v2) {
+        return "YES" 
+    } 
+
+
+}
+
+console.log(kangaroo(0, 2, 5, 3))
+console.log(kangaroo(0, 3, 4, 2))
