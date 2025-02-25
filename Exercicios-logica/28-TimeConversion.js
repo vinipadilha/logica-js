7469cfcb0ee268865dba630b6fec7e213eec58e3
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-25 00:51:59 -0300

    exercicio feito de maneira errada, trocando somente de am para pm

--- Exercicios-logica/28-TimeConversion.js
+++ Exercicios-logica/28-TimeConversion.js
@@ -1,7 +1,18 @@
 function timeConversion(s) {
     // Write your code here
+    let hr = s.slice(0, 2)
+    let newhr = s.slice(2, -2)
+
+    if(hr >= 12){
+        hr = hr - 12
+    }
+    return console.log(hr + newhr + "PM")
+
 
 
 }
 
 console.log(timeConversion("12:00:00AM"))
+console.log(timeConversion("07:05:45PM"))
+
+function timeConversion
