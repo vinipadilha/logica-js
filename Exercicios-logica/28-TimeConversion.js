b9ea7bc20b7d93f8a8641af6131dace7b32d2e36
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-25 01:02:34 -0300

    primeira tentativa

--- Exercicios-logica/28-TimeConversion.js
+++ Exercicios-logica/28-TimeConversion.js
@@ -1,18 +1,19 @@
-function timeConversion(s) {
-    // Write your code here
-    let hr = s.slice(0, 2)
+
+function timeConversion(s){
+
+    let format = s.slice(-2)
+    let hr = Number(s.slice(0, 2))
     let newhr = s.slice(2, -2)
 
-    if(hr >= 12){
-        hr = hr - 12
+    if(format == "PM"){
+        hr = hr + 12
+        return hr + newhr
+
+    } else{
+        return s.slice(0, -2)
     }
-    return console.log(hr + newhr + "PM")
-
-
-
 }
 
-console.log(timeConversion("12:00:00AM"))
+
 console.log(timeConversion("07:05:45PM"))
 
-function timeConversion
