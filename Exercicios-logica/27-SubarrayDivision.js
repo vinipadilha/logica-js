037db0c2226bf078d6966c91d8dcdf8e5b18c9e5
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-02-27 00:35:57 -0300

    primeira tentativa do birthday ex

---
+++ Exercicios-logica/27-SubarrayDivision.js
@@ -0,0 +1,26 @@
+function birthday(s, d, m) {
+    // Write your code here
+    let count = 0;
+    let sum = 0;
+
+    for (let i = 0; i <= (s.length - m); i++) {
+
+        for (let index2 = 0; index2 < m; index2++) {
+
+            sum = sum + s[i + index2]; 
+        }
+
+        if(sum === d){
+            count += 1;
+            
+        }
+        sum = 0;
+    }
+    return console.log(count)
+}
+
+birthday([2, 2, 1, 3, 2], 4, 2)
+
+
+
+
