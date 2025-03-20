0c8aff9eeda226b3f409a703703f40a7d5f0017f
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-03-19 23:17:57 -0300

    Define variaveis da função timeConversion

--- Exercicios-logica/28-TimeConversion.js
+++ Exercicios-logica/28-TimeConversion.js
@@ -1,6 +1,11 @@
 function timeConversion(s){
 
-        
+    s.split('');
+
+    let hours = parseInt(s[0] + s[1]);
+    let minutes = s[3] + s[4];
+    let seconds = s[6] + s[7];
+    let ampm = s[s.lenght - 2]
 }
 
 
