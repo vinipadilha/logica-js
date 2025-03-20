fa34147e2a36fecb29ab5dad74a9f75a62cf062b
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-03-19 23:08:49 -0300

    Reinicia exercicio de time conversion

--- Exercicios-logica/28-TimeConversion.js
+++ Exercicios-logica/28-TimeConversion.js
@@ -1,23 +1,11 @@
-
 function timeConversion(s){
 
-    let format = s.slice(-2)
-    let hr = Number(s.slice(0, 2))
-    let newhr = s.slice(2, -2)
-
-    if(format == "PM"){
-        hr = hr + 12
-        return hr + newhr
-
-    } else if(format == "AM" || 12){
-        hr = hr - 12
-        return hr + newhr
-
-    } else{
-        return s.slice(0, -2)
-    }
+        
 }
+
+
 console.log(timeConversion("07:05:45PM"))
 console.log(timeConversion("07:05:45AM"))
 console.log(timeConversion("12:05:39AM"))
+console.log(timeConversion("06:40:03AM"))
 
